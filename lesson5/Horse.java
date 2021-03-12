fb791b1c520904ee6a0f11c9332e5e99c35544dc
Oleg Rybchenko (ORBS) <69960367+RybchenkoOV@users.noreply.github.com>
2021-03-12 21:57:34 +0300

    Delete lesson5 directory

--- lesson5/Horse.java
+++
@@ -1,9 +0,0 @@
-package lesson5;
-
-public class Horse extends Animal{
-
-    public Horse (String name, int abRun, int abSwim, float abJump) {
-        super(name, abRun, abSwim, abJump);
-    }
-
-}
